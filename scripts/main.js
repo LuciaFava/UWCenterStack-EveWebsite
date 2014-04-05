c530f57cf3efb7848f47e8af8e44de6b82e33b9c
Mitchell Loeppky <mitchell@loeppky.com>
2014-04-05 16:41:32 -0700

    Added JS for navigation and finished home implementation
    Also converted tabs to spaces

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
 /*
 main.js
 */
+
+(function() {
+    $(window).scroll(function() {
+        // console.log('about scrollTop: ' + $('#about').scrollTop());
+        if ($(window).height() < ($(window).scrollTop() + $('#navBar').outerHeight())) {
+            $('#navBar').css('position', 'fixed').css('top', 0).css('bottom', 'auto');
+        } else {
+            $('#navBar').css('position', 'absolute').css('top', 'auto').css('bottom', 0);
+        }
+    });
+
+    $('.navButton').each(function() {
+        $(this).click(function() {
+            var target = $('#' + $(this).attr('data-target'));
+            $('body, html').animate({scrollTop: target.offset().top - $('#navBar').outerHeight()}, 500);
+        }); 
+    })
+})()
